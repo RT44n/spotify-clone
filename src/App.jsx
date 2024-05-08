fdfb86f9ab4b2f6794b191c0074d5be09075010d
RT44n <147970845+RT44n@users.noreply.github.com.>
2024-05-08 17:57:46 +0530

    add sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 import "./App.css";
+import SideBar from "./components/Sidebar";
 
-function App() {}
+function App() {
+  return (
+    <>
+      <div className="h-screen bg-black">
+        <div className="h-[90%] flex">
+          <SideBar></SideBar>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default App;
